0064645654e5dfce99eea23409e5bda7a5bde2fa
Ян Нестеров <116672081+YanL1ght@users.noreply.github.com>
2023-12-19 17:00:32 +0300

    добавлены комментарии в Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -1,8 +1,8 @@
 import pygame as pg
 
 
-class Bullet:
-    def __init__(self, x, y, radius, color, facing):
+class Bullet:  # класс пули
+    def __init__(self, x, y, radius, color, facing):  # создание пули по введённым параметрам
         self.x = x
         self.y = y
         self.radius = radius
@@ -10,5 +10,6 @@ class Bullet:
         self.facing = facing
         self.vel = 50 * facing
 
-    def render(self, sc):
+    def render(self, sc):  # отрисовка пули
         pg.draw.circle(sc, self.color, (self.x, self.y), self.radius)
+        
